251dd67c31f599ee29bfa302fa2e83c6c087b7b0
yangzhao98 <yangz98@connect.hku.hk>
2024-08-28 23:52:27 +0800

    add MIT license

--- R/utilities.R
+++ R/utilities.R
@@ -1,30 +1,33 @@
 # offline_installer.R
 
-download_r_packages <- function(cran_pkgs = NULL, github_repos = NULL, bioc_pkgs = NULL, dest_dir) {
+download_r_packages <- function(cran_pkgs=NULL,
+                                github_repos=NULL,
+                                bioc_pkgs=NULL,
+                                dest_dir) {
   if (!dir.exists(dest_dir)) {
     dir.create(dest_dir, recursive = TRUE)
   }
 
   if (!is.null(cran_pkgs)) {
-    utils::install.packages(cran_pkgs, lib = dest_dir, dependencies = TRUE, type = "source", repos = "https://cran.rstudio.com/")
-    utils::download.packages(cran_pkgs, destdir = dest_dir, type = "source", repos = "https://cran.rstudio.com/")
+    utils::install.packages(cran_pkgs, lib=dest_dir, dependencies=TRUE, type="source", repos="https://cran.rstudio.com/")
+    utils::download.packages(cran_pkgs, destdir=dest_dir, type="source", repos="https://cran.rstudio.com/")
   }
 
   if (!is.null(github_repos)) {
     for (repo in github_repos) {
-      devtools::install_github(repo, lib = dest_dir, dependencies = TRUE, upgrade = "never", INSTALL_opts = "--no-multiarch", type = "source")
+      devtools::install_github(repo, lib=dest_dir, dependencies=TRUE, upgrade="never", INSTALL_opts="--no-multiarch", type="source")
     }
     for (repo in github_repos) {
-      pkgs <- remotes::package_deps(repo, repos = "https://cran.rstudio.com/", dependencies = TRUE)
-      utils::download.packages(pkgs$package, destdir = dest_dir, type = "source", repos = "https://cran.rstudio.com/")
+      pkgs <- remotes::package_deps(repo, repos="https://cran.rstudio.com/", dependencies=TRUE)
+      utils::download.packages(pkgs$package, destdir=dest_dir, type="source", repos="https://cran.rstudio.com/")
     }
   }
 
   if (!is.null(bioc_pkgs)) {
-    BiocManager::install(bioc_pkgs, lib = dest_dir, dependencies = TRUE, ask = FALSE, update = FALSE)
-    installed_pkgs <- utils::installed.packages(lib.loc = dest_dir)
+    BiocManager::install(bioc_pkgs, lib=dest_dir, dependencies=TRUE, ask=FALSE, update=FALSE)
+    installed_pkgs <- utils::installed.packages(lib.loc=dest_dir)
     for(pkg in rownames(installed_pkgs)) {
-      utils::download.packages(pkg, destdir = dest_dir, type = "source", repos = "https://cran.rstudio.com/")
+      utils::download.packages(pkg, destdir=dest_dir, type="source", repos="https://cran.rstudio.com/")
     }
   }
 }
@@ -51,10 +54,12 @@ generate_install_script <- function(dest_dir) {
 #' @param bundle_name name of the pkgs
 #' @import utils
 #' @export
-package_offline_bundle <- function(cran_pkgs = NULL, github_repos = NULL, bioc_pkgs = NULL, dest_dir, bundle_name) {
+package_offline_bundle <- function(cran_pkgs=NULL,
+                                   github_repos=NULL,
+                                   bioc_pkgs=NULL, dest_dir, bundle_name) {
   download_r_packages(cran_pkgs, github_repos, bioc_pkgs, dest_dir)
   generate_install_script(dest_dir)
   zip_file <- paste0(bundle_name, ".zip")
-  zip(zipfile = zip_file, files = list.files(dest_dir, full.names = TRUE))
+  zip(zipfile=zip_file, files=list.files(dest_dir, full.names=TRUE))
   cat("Generate offline pkgs: ", zip_file, "\n")
 }
